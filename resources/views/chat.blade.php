e74196a2c632ada67d95a6f9148426fe42aab8a0
PrionX-svg <oliviasalma06@gmail.com>
2024-06-01 00:02:24 +0700

    Create the header in chat section

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -9,7 +9,16 @@
     <title>Chat</title>
 </head>
 <body>
-    
+    <header>
+        <div class="left">
+            <div id="backButton"></div>
+            <h2>Chat</h2>
+        </div>
+        <div class="right">
+            <div id="notifButton"></div>
+            <div id="profileButton"></div>
+        </div>
+    </header>
 </body>
 </html>
 @endsection
