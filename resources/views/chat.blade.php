c18cc0937d1314f47f29039a9288430d4c569c9e
PrionX-svg <oliviasalma06@gmail.com>
2024-05-31 23:54:13 +0700

    Create and update chat page
    Buat chat page lagi, karna tadi clone ulang di folder yang diinginkan

---
+++ resources/views/chat.blade.php
@@ -0,0 +1,15 @@
+@extends('layout')
+@extends('title')
+@section('chat_content')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chat</title>
+</head>
+<body>
+    
+</body>
+</html>
+@endsection
