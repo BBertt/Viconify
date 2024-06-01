d875160cda09d7b064c6d91e4cdc234f1cb25c42
Wilson <163714739+NiceGhoul@users.noreply.github.com>
2024-06-01 21:11:50 +0700

    delete

--- resources/js/utils/cn.ts
+++
@@ -1,8 +0,0 @@
-// resources/js/utils/cn.ts
-
-import { ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
